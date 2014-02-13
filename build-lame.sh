47d692fe0c675985b8b3bae820e972ac30086b22
Changbeom Ahn <kewlbear@gmail.com>
2014-02-13 11:01:16 +0900

    initial version

---
+++ build-lame.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+CONFIGURE_FLAGS="--disable-shared --disable-frontend"
+
+ARCHS="arm64 armv7s x86_64 i386 armv7"
+
+# directories
+SOURCE="lame"
+FAT="fat-lame"
+
+SCRATCH="scratch-lame"
+# must be an absolute path
+THIN=`pwd`/"thin-lame"
+
+COMPILE="y"
+LIPO="y"
+
+if [ "$*" ]
+then
+	if [ "$*" = "lipo" ]
+	then
+		# skip compile
+		COMPILE=
+	else
+		ARCHS="$*"
+		if [ $# -eq 1 ]
+		then
+			# skip lipo
+			LIPO=
+		fi
+	fi
+fi
+
+if [ "$COMPILE" ]
+then
+	CWD=`pwd`
+	for ARCH in $ARCHS
+	do
+		echo "building $ARCH..."
+		mkdir -p "$SCRATCH/$ARCH"
+		cd "$SCRATCH/$ARCH"
+
+		if [ "$ARCH" = "i386" -o "$ARCH" = "x86_64" ]
+		then
+		    PLATFORM="iPhoneSimulator"
+		    if [ "$ARCH" = "x86_64" ]
+		    then
+		    	SIMULATOR="-mios-simulator-version-min=7.0"
+                        HOST=x86_64-apple-darwin
+		    else
+		    	SIMULATOR="-mios-simulator-version-min=5.0"
+                        HOST=i386-apple-darwin
+		    fi
+		else
+		    PLATFORM="iPhoneOS"
+		    SIMULATOR=
+                    HOST=arm-apple-darwin
+		fi
+
+		XCRUN_SDK=`echo $PLATFORM | tr '[:upper:]' '[:lower:]'`
+		CC="xcrun -sdk $XCRUN_SDK clang -arch $ARCH"
+		#AS="$CWD/$SOURCE/extras/gas-preprocessor.pl $CC"
+		CFLAGS="-arch $ARCH $SIMULATOR"
+		CXXFLAGS="$CFLAGS"
+		LDFLAGS="$CFLAGS"
+
+		CC=$CC $CWD/$SOURCE/configure \
+		    $CONFIGURE_FLAGS \
+                    --host=$HOST \
+		    --prefix="$THIN/$ARCH" \
+                    CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
+
+		make -j3 install
+		cd $CWD
+	done
+fi
+
+if [ "$LIPO" ]
+then
+	echo "building fat binaries..."
+	mkdir -p $FAT/lib
+	set - $ARCHS
+	CWD=`pwd`
+	cd $THIN/$1/lib
+	for LIB in *.a
+	do
+		cd $CWD
+		lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB
+	done
+
+	cd $CWD
+	cp -rf $THIN/$1/include $FAT
+fi
